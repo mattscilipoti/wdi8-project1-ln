6d08f2dd2cf36e226579eae05f1586ee89ace9ac
Lyndsey Newsome <lyndseysnewsome@gmail.com>
2016-02-22 13:00:34 -0500

    Testing if loop.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var numbers;
 var playerAnswer;
 var add;
+var answerCheck;
 
 prompt ("Solve 10 + 2");
 
@@ -9,6 +10,6 @@ function add (num1, num2) {
 }
  if (playerAnswer == add){
      document.write ("You're Correct");
- }else {
+ }else (playerAnswer!= add){
      document.write("Sorry! Maybe next time!");
  }
