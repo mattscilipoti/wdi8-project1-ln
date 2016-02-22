a4955a95989004d2be7122c09ec2c7d377738b7d
Lyndsey Newsome <lyndseysnewsome@gmail.com>
2016-02-22 15:38:02 -0500

    Simplified all function, varibles and index. Created arrays and loop.

--- script.js
+++ script.js
@@ -1,40 +1,20 @@
-var numbers = [1,2,3,4,5,6,7,8,9,10];
-var playerAnswer;
 var add;
-// var answerCheck;
+var questions = ["1+1", "2+6", "10+9", "3+7", "PBJ?"];
+var answers = [2, 8, 19, 10, "Peanut Butter and Jelly"];
 
 
-// prompt random question to player
-// var solve = prompt("Solve 1+ 2");
-
-
-// Funtion for players to insert answer.
-var add;
-
-prompt ("1+1");
-    function add (num1, num2){
-        return num1+num2;
+function add (num1, num2){
+    return num1+num2;
 }
-add (1,1) ;
-    alert("2");
 
-prompt ("2+6");
-    function add (num1, num2){
-        return num1+num2;
-}
-add (2, 6);
-    alert("8");
+// loop thru itmes in questins
+for(var questionIndex = 0; questionIndex < questions.length; questionIndex ++){
 
-prompt ("10+9");
-    function add (num1, num2){
-        return num1+num2;
-}
-add (10, 9);
-    alert("19");
-// Prompt for correct or incorrect answer.
+    var question = questions[questionIndex];
 
- // if (playerAnswer == add){
-    //  alert("You're Correct");
- // }else (playerAnswer!= add){
-    //  alert("Sorry! Maybe next time!");
- // }
+    prompt(question);
+    // var answer = eval(question);
+    var answer = answers[questionIndex];
+    alert(answer);
+
+// end of loop
