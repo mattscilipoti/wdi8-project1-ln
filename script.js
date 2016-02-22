4345179bf97fbab3be64aa82cc5f59a7a30801c9
Lyndsey Newsome <lyndseysnewsome@gmail.com>
2016-02-22 12:24:08 -0500

    Started with simple javascript. Add function for playerAnswer insert.

---
+++ script.js
@@ -0,0 +1,14 @@
+var numbers;
+var playerAnswer;
+var add;
+
+prompt ("Solve 10 + 2");
+
+function add (num1, num2) {
+    return num1 + num2;
+}
+ if (playerAnswer == add){
+     document.write ("You're Correct");
+ }else {
+     document.write("Sorry! Maybe next time!");
+ }
