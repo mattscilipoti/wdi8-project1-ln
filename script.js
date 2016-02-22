6f05c111dd0f177478b4ffa1f015def7a8e10ea1
Lyndsey Newsome <lyndseysnewsome@gmail.com>
2016-02-22 13:09:27 -0500

    Changed document.word(?) to alert.

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ var playerAnswer;
 var add;
 var answerCheck;
 
-prompt ("Solve 10 + 2");
+var solve = prompt("Solve 1+ 2");
 
 function add (num1, num2) {
     return num1 + num2;
 }
  if (playerAnswer == add){
-     document.write ("You're Correct");
+     alert("You're Correct");
  }else (playerAnswer!= add){
-     document.write("Sorry! Maybe next time!");
+     alert("Sorry! Maybe next time!");
  }
