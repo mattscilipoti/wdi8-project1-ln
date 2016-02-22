1b7428de04878d64e1dd2646e11108c1a5c1b096
Lyndsey Newsome <lyndseysnewsome@gmail.com>
2016-02-22 14:53:11 -0500

    Simplified projects into 3 functions, prompts and alerts.

--- script.js
+++ script.js
@@ -1,15 +1,40 @@
-var numbers;
+var numbers = [1,2,3,4,5,6,7,8,9,10];
 var playerAnswer;
 var add;
-var answerCheck;
+// var answerCheck;
 
-var solve = prompt("Solve 1+ 2");
 
-function add (num1, num2) {
-    return num1 + num2;
+// prompt random question to player
+// var solve = prompt("Solve 1+ 2");
+
+
+// Funtion for players to insert answer.
+var add;
+
+prompt ("1+1");
+    function add (num1, num2){
+        return num1+num2;
 }
- if (playerAnswer == add){
-     alert("You're Correct");
- }else (playerAnswer!= add){
-     alert("Sorry! Maybe next time!");
- }
+add (1,1) ;
+    alert("2");
+
+prompt ("2+6");
+    function add (num1, num2){
+        return num1+num2;
+}
+add (2, 6);
+    alert("8");
+
+prompt ("10+9");
+    function add (num1, num2){
+        return num1+num2;
+}
+add (10, 9);
+    alert("19");
+// Prompt for correct or incorrect answer.
+
+ // if (playerAnswer == add){
+    //  alert("You're Correct");
+ // }else (playerAnswer!= add){
+    //  alert("Sorry! Maybe next time!");
+ // }
