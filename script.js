b52ba79feaf7aeb0747a92e36b578e6afee0f65f
Lyndsey Newsome <lyndseysnewsome@gmail.com>
2016-02-23 17:04:14 -0500

    added button function. changed html.

--- script.js
+++ script.js
@@ -2,41 +2,34 @@ var add;
 var questions = ["1+1", "2+6", "10+9", "3+7", "PBJ?"];
 var answers = [2, 8, 19, 10, "Peanut Butter and Jelly"];
 var answerButton = document.getElementById("answerButton");
-var questionButton =document.getElementById("questionButton");
+var nextQuestionButton =document.getElementById("nextQuestionButton");
 
 
-function add (num1, num2){
-    return num1+num2;
-}
 
 // for(var questionsIndex = 0; questionsIndex < questions.length; questionsIndex ++){
     var questionsIndex = 0;
+    displayQuestion(questionsIndex);
 
+
+function displayQuestion(questionIndex){
     // fills in page
-    var question = questions[questionsIndex];
+    var question = questions[questionIndex];
     // alert(question);
     var questionTag = document.getElementById ("question")
     questionTag.textContent = question;
 
-    var answer = answers[questionsIndex];
+    var answer = answers[questionIndex];
     var answerTag = document.getElementById("answer")
     answerTag.textContent = answer;
     // prompt(answer);
 
-  // }
+}
 
 answerButton.addEventListener("click", function(){
   alert("Here's your anwer!");
 });
-questionButton.addEventListener("click", function (){
+nextQuestionButton.addEventListener("click", function (){
   questionsIndex ++
-  // fill in page
-  var question = questions[questionsIndex];
-  // alert(question);
-  var questionTag = document.getElementById ("question")
-  questionTag.textContent = question;
+  displayQuestion(questionsIndex);
 
-  var answer = answers[questionsIndex];
-  var answerTag = document.getElementById("answer")
-  answerTag.textContent = answer;
 });
